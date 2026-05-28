da887b41a068bb4193030893f7498bec89317b7d
ahmedelsefir <135118337+ahmedelsefir@users.noreply.github.com>
2026-05-28 06:13:10 +0300

    Update assistant.py
    Signed-off-by: ahmedelsefir <135118337+ahmedelsefir@users.noreply.github.com>

--- assistant.py
+++ assistant.py
@@ -18,8 +18,7 @@ except Exception:
     FIREBASE_URL = "https://gen-lang-client-03099029-937be-default-rtdb.firebaseio.com"
     ZOHO_EMAIL = "ahmed.mustafa@monjez-app.icu"
     ZOHO_PASSWORD = "42s1kTKByngN"
-    ZOHO_WEBHOOK_URL = """https://flow.zoho.com/925590557/flow/webhook/incoming?zapikey=1001.75117eafd3bfa3fdfdb995b579271db5.78450badb7fc8219cb67a6213ae24197&isdebug=false"""
-
+   
 # دالة مخصصة لشحن (حفظ) البيانات في Firebase
 def save_to_firebase(node, data):
     try:
